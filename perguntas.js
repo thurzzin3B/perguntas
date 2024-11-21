eeededf951b5a82a1128f8796b5b81cd0139fe2a
thurzzin3B <168560146+thurzzin3B@users.noreply.github.com>
2024-11-21 11:26:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matematica',
+    'Quanto é 60 ÷ 5 ( 3 + 3 ( 3 + 3 )) =❓',
+    'A resposta é 252'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem foi a maior jogador dos ultimos tempos ?',
+    'Messi'
+)
+
+criaCartao(
+    'Cinema',
+    'Nos filmes Transformes, qual é o nome dos robôs malvados ?',
+    'Decepticons'
+)
+
+criaCartao(
+    'matematica',
+    'Quanto é 7 ̶ 2 (1 + 3)?',
+    'A resposta é -1'
+)
